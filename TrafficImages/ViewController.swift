e3a4c4f329223e86199f751a67d491992f74dace
Jeet Gandhi <jeet_gandhi@singaporeair.com.sg>
2020-12-13 15:23:59 +0800

    Initial Commit

---
+++ TrafficImages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficImages
+//
+//  Created by jeet_gandhi on 13/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
